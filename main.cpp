13083140b4982966bee9a3cb1ef9eb214d16fe8a
Asad Mehmood <asadmehmood96@gmail.com>
2025-02-20 00:49:24 +0500

    My first push

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std; 
+
+int main(int argc, char** argv) {
+	
+	int x=5;
+	int y=4;
+	
+	int a=x+y;
+	cout<< a;
+
+	
+	return 0;
+}
