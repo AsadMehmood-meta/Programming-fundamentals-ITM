97b862c0b39f2ed76d85814f4e4a43aa9ed98184
Asad Mehmood <114588273+AsadMehmood-meta@users.noreply.github.com>
2025-02-20 19:23:22 +0500

    Add files via upload
    20-02-2025

--- main.cpp
+++ main.cpp
@@ -1,16 +1,74 @@
 #include <iostream>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-using namespace std; 
+using namespace std;
 
 int main(int argc, char** argv) {
 	
-	int x=5;
-	int y=4;
-	
-	int a=x+y;
-	cout<< a;
+//odd and even number//
+////	int n1;
+////	cout<< "Enter any number: ";
+////	cin>>n1;
+////	double n2 = n1%2;
+////	if (n2==0){
+////		cout<<"Number is Even";
+////	} else {
+////		cout<< "Number is Odd";
+////	}
 
+//Which number is positive or negative
+////	int n1;
+////	int n2;
+////	cout<<"Enter first number: ";
+////	cin>>n1;
+////	cout<<"Enter second number";
+////	cin>>n2;
+////	int n3=n1+n2;
+////	if (n3<0){
+////		cout<<n3 <<" is negative number";
+////	} else if (n3>0){
+////		cout<<n3 << " is positive number";
+////	} else {
+////		cout<<"number is 0";
+////	}
+
+//Second Largest Number
+//	int n1;
+//	int n2;
+//	int n3;
+//	cout<<"Enter first number: ";
+//	cin>>n1;
+//	cout<<"Enter second number: ";
+//	cin>>n2;
+//	cout<<"Enter third number: ";
+//	cin>>n3;	
+//	if (n1>n2 && n1<n3 || n1<n2 && n1>n3){
+//		cout<<n1<<" is second largest number";
+//	} else if (n2>n1&&n2<n3 || n2<n1&&n2>n3){
+//		cout<<n2<<" is second largest number";
+//	} else {
+//		cout<<n3<<" is second largest number";
+//	}
+
+////Grade	
+//	int m2;
+//	cout<<"Enter marks: ";
+//	cin>>m2;
+//	if (m2>=90&&m2<=100){
+//		cout<<"A Grade";
+//	} else if (m2>=80&&m2<90){
+//		cout<<"B Grade";
+//	} else if (m2>=70&&m2<80){
+//		cout<<"C Grade";
+//	} else if (m2>=60&&m2<70){
+//		cout<<"D Grade";
+//	} else if (m2>=0&&m2<60) {
+//		cout<<"Fail";
+//	} else {
+//		cout<<"Invalid Marks";
+//	}
+	
+	
 	
 	return 0;
 }
