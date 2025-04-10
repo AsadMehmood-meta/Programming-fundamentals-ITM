111fcbddd8add7465e18864f36489f6f213ec8a0
Asad Mehmood <114588273+AsadMehmood-meta@users.noreply.github.com>
2025-04-10 20:16:25 +0500

    Add files via upload

---
+++ 10-04-2025.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+//void name (string a){
+//	cout<<"Name: "<<a<<"\t\t";
+//}
+//
+//void name (char a){
+//	cout<<"Gender: "<<a;
+//}
+//*****************************************
+
+	
+
+int main(){
+
+//*****************************************
+
+//	string x;
+//	char y;
+//
+//	cout<<"Enter your name"<<endl;
+//	getline(cin,x);
+//		
+//	cout<<"Enter your gender (M or F)"<<endl;
+//	cin>>y;
+//
+//	name(x);
+//	name(y);
+
+//*****************************************
+
+	
+		
+	
+}
